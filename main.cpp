ac0da04db79bb6d03598d575a1291b90ce3027c6
Jakub Dębosz <jakubdebosz.it@gmail.com>
2023-03-11 13:02:24 +0100

    Added sample hash function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string.h>
+
+#define CAPACITY 50000
+
+unsigned long hash_function(char* str) {
+    
+    unsigned long result = 0;
+    for (int i = 0; str[i]; i++) {
+        result += str[i];
+    }
+
+    return result % CAPACITY;
+}
 
 int main() {
-
-    std::cout << "Test";
+    printf("Calculated index for value 'Test': %i\n", hash_function((char*)"Test"));
+    printf("Calculated index for value 'Tset': %i\n", hash_function((char*)"Tset"));
     return 0;
-
 }
